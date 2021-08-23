c0c9681ed8d62a1c1ed6b0e8fddb85bcc12b9fd8
Wiput Pootong <wiput.pootong@gmail.com>
2021-08-24 01:20:05 +0700

    refactor: theme

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -39,7 +39,7 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'dark',
+      theme: 'light',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
